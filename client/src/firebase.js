332d9b7c8f91ff74150ffcc0e84ab5a3537727c4
Haider Ali <ha5743330@gmail.com>
2026-06-24 21:00:03 +0500

    deployment fix

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,24 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // <--- Ye line add ki hai
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQP_x97H2DZng5tFsia9iBWILozC7kmg4",
-  authDomain: "web-development-internsh-87db8.firebaseapp.com",
-  projectId: "web-development-internsh-87db8",
-  storageBucket: "web-development-internsh-87db8.firebasestorage.app",
-  messagingSenderId: "663193515525",
-  appId: "1:663193515525:web:67ebc799382ce76e198e6c",
-  measurementId: "G-GVC4PRTWCK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialize Firestore, Auth and EXPORT them
-export const db = getFirestore(app);
-export const auth = getAuth(app); // <--- Ye export zaroori hai
